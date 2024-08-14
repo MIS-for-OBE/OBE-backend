ac7e5a0548dbfebfb02c2c6bc7ae51c3fe90b20d
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-08-14 21:55:40 +0700

    #fixed: getPLOs

--- src/obe/plo/module.ts
+++ src/obe/plo/module.ts
@@ -6,6 +6,7 @@ import { UserModel } from '../user/module';
 import { PLO, PLOSchema } from './schemas/schema';
 import { LogEventModel } from '../logEvent/module';
 import { LogEventService } from '../logEvent/service';
+import { FacultyModel } from '../faculty/module';
 
 export const PLOModel = {
   name: PLO.name,
@@ -16,6 +17,7 @@ export const PLOModel = {
   imports: [
     MongooseModule.forFeature([
       PLOModel,
+      FacultyModel,
       UserModel,
       // CourseModel,
       // CourseManagementModel,
--- src/obe/plo/service.ts
+++ src/obe/plo/service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PLO } from './schemas/schema';
-import { Model } from 'mongoose';
+import { Collection, Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { ROLE } from 'src/common/enum/role.enum';
+import { Faculty } from '../faculty/schemas/schema';
 
 @Injectable()
 export class PLOService {
   constructor(
     @InjectModel(PLO.name) private readonly model: Model<PLO>,
+    @InjectModel(Faculty.name) private readonly facultyModel: Model<Faculty>,
     // @InjectModel(Course.name) private readonly courseModel: Model<Course>,
     // @InjectModel(CourseManagement.name)
     // private readonly courseManagementModel: Model<CourseManagement>,
@@ -18,11 +20,31 @@ export class PLOService {
       if (searchDTO.role !== ROLE.SUPREME_ADMIN) {
         where.departmentCode = { $in: searchDTO.departmentCode };
       }
+      const faculty = await this.facultyModel.findOne({ facultyCode });
       const totalCount = await this.model.countDocuments(where);
-      const plos = await this.model.find(where).sort([
+      const data = await this.model.find(where).sort([
         ['year', 'desc'],
         ['semester', 'desc'],
       ]);
+      const plos = searchDTO.departmentCode.map((dep) => {
+        return {
+          departmentCode: dep,
+          departmentEN: faculty.department.find(
+            (code) => code.departmentCode == dep,
+          ).departmentEN,
+          collections: [],
+        };
+      });
+      plos.forEach((plo, index) => {
+        data.forEach((collection) => {
+          if (collection.departmentCode.includes(plo.departmentCode)) {
+            plo.collections.push(collection);
+          }
+        });
+        if (!plo.collections.length) {
+          plos.splice(index);
+        }
+      });
       return { totalCount, plos };
     } catch (error) {
       throw error;
