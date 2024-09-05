e89c26fb8eb7b414e3c49859ffd4527a47e0aee5
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-09-05 21:26:41 +0700

    #fixed: saveTQF3

--- src/obe/tqf3/service.ts
+++ src/obe/tqf3/service.ts
@@ -1,21 +1,18 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { TQF3 } from './schemas/schema';
 import { Model } from 'mongoose';
+import { TQF_STATUS } from 'src/common/enum/type.enum';
 
 @Injectable()
 export class TQF3Service {
   constructor(@InjectModel(TQF3.name) private readonly model: Model<TQF3>) {}
-  
+
   async savePart1(id: string, requestDTO: any): Promise<TQF3> {
     try {
       const updateTQF3 = await this.model.findByIdAndUpdate(
         id,
-        { part1: requestDTO },
+        { status: TQF_STATUS.IN_PROGRESS, part1: requestDTO },
         { new: true, fields: 'part1 updatedAt' },
       );
       if (!updateTQF3) {
