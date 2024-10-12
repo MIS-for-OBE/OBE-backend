25f35eef8e859f7f1080ccf67849454801ab75a2
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-10-12 20:15:12 +0700

    #create: assignment, question, score schema

--- src/obe/section/schemas/section.schema.ts
+++ src/obe/section/schemas/section.schema.ts
@@ -1,14 +1,61 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-import { Course } from 'src/obe/course/schemas/course.schema';
 import { PLONo } from 'src/obe/plo/schemas/plo.schema';
-// import { Assignment } from 'src/obe/assignment/schemas/assignment.schema';
 import { TQF3 } from 'src/obe/tqf3/schemas/tqf3.schema';
 import { TQF5 } from 'src/obe/tqf5/schemas/tqf5.schema';
 import { User } from 'src/obe/user/schemas/user.schema';
 
-export type SectionDocument = HydratedDocument<Section>;
+@Schema()
+export class Score {
+  @Prop({
+    unique: true,
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  })
+  student: User;
 
+  @Prop({ required: true })
+  point: number;
+}
+export const ScoreSchema = SchemaFactory.createForClass(Score);
+
+@Schema()
+export class Question {
+  @Prop({ unique: true, required: true })
+  no: number;
+
+  @Prop()
+  desc: string;
+
+  @Prop({ required: true })
+  fullScore: number;
+
+  @Prop({ type: [ScoreSchema], _id: false, required: true })
+  scores: Score[];
+}
+export const QuestionSchema = SchemaFactory.createForClass(Question);
+
+@Schema()
+export class Assignment {
+  @Prop({ unique: true, required: true })
+  name: string;
+
+  @Prop()
+  desc: string;
+
+  @Prop({ default: false })
+  isPublish: string;
+
+  @Prop({ required: true })
+  weight: number;
+
+  @Prop({ type: [QuestionSchema], _id: false, required: true })
+  questions: Question[];
+}
+export const AssignmentSchema = SchemaFactory.createForClass(Assignment);
+
+export type SectionDocument = HydratedDocument<Section>;
 @Schema()
 export class Section {
   @Prop({ required: true })
@@ -20,32 +67,19 @@ export class Section {
   @Prop({ required: true, default: true })
   isActive: boolean;
 
-  @Prop({
-    required: true,
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  })
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'User' })
   instructor: User;
 
-  @Prop({
-    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  })
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] })
   coInstructors: User[];
 
-  // @Prop({
-  //   type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Assignment' }],
-  // })
-  // assignments: Assignment[];
-
-  @Prop({ type: [] })
-  assignments: any[];
+  @Prop({ type: [AssignmentSchema], _id: false })
+  assignments: Assignment[];
 
   @Prop()
   topic: string;
 
-  @Prop({
-    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PLONo' }],
-  })
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PLONo' }] })
   plos: PLONo[];
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'TQF3' })
@@ -54,7 +88,6 @@ export class Section {
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'TQF5' })
   TQF5: TQF5;
 }
-
 export const SectionSchema = SchemaFactory.createForClass(Section);
 
 // SectionSchema.pre('findOneAndDelete', async function (next) {
