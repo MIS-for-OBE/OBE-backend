c70ee5fdb3f4d77a77b39440089ef552a12f3662
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-11-11 13:42:08 +0700

    #feature: recurrence course

--- src/obe/academicYear/academicYear.module.ts
+++ src/obe/academicYear/academicYear.module.ts
@@ -6,11 +6,19 @@ import {
   AcademicYear,
   AcademicYearSchema,
 } from './schemas/academicYear.schema';
+import { Course, CourseSchema } from '../course/schemas/course.schema';
+import { CourseManagement, CourseManagementSchema } from '../courseManagement/schemas/courseManagement.schema';
+import { TQF3, TQF3Schema } from '../tqf3/schemas/tqf3.schema';
+import { TQF5, TQF5Schema } from '../tqf5/schemas/tqf5.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: AcademicYear.name, schema: AcademicYearSchema },
+      { name: Course.name, schema: CourseSchema },
+      { name: CourseManagement.name, schema: CourseManagementSchema },
+      { name: TQF3.name, schema: TQF3Schema },
+      { name: TQF5.name, schema: TQF5Schema },
     ]),
   ],
   controllers: [AcademicYearController],
--- src/obe/academicYear/academicYear.service.ts
+++ src/obe/academicYear/academicYear.service.ts
@@ -3,11 +3,21 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AcademicYear } from './schemas/academicYear.schema';
 import { AcademicYearSearchDTO } from './dto/search.dto';
+import { Course, Section } from '../course/schemas/course.schema';
+import { CourseManagement } from '../courseManagement/schemas/courseManagement.schema';
+import { COURSE_TYPE, TQF_STATUS } from 'src/common/enum/type.enum';
+import { TQF5 } from '../tqf5/schemas/tqf5.schema';
+import { TQF3 } from '../tqf3/schemas/tqf3.schema';
 
 @Injectable()
 export class AcademicYearService {
   constructor(
     @InjectModel(AcademicYear.name) private readonly model: Model<AcademicYear>,
+    @InjectModel(Course.name) private readonly courseModel: Model<Course>,
+    @InjectModel(CourseManagement.name)
+    private readonly courseManagementModel: Model<CourseManagement>,
+    @InjectModel(TQF3.name) private readonly tqf3Model: Model<TQF3>,
+    @InjectModel(TQF5.name) private readonly tqf5Model: Model<TQF5>,
   ) {}
 
   async searchAcademicYear(
@@ -50,12 +60,78 @@ export class AcademicYearService {
         { isActive: true },
         { isActive: false },
       );
-      const res = await this.model.findByIdAndUpdate(
+      const academic = await this.model.findByIdAndUpdate(
         academicYearId,
         { isActive: true },
         { new: true },
       );
-      return res;
+      const courseManagements = await this.courseManagementModel.find({
+        sections: {
+          $elemMatch: { semester: academic.semester },
+        },
+      });
+
+      const newCourses: Course[] = [];
+      for (const course of courseManagements) {
+        for (const section of course.sections) {
+          const isCreate = section.semester.includes(academic.semester);
+          const dataSection: any = { ...(section as any)._doc };
+          let existingCourse = newCourses.find(
+            (item) => item.courseNo == course.courseNo,
+          );
+          if (!existingCourse && isCreate) {
+            const newCourse: Partial<Course> = {
+              year: academic.year,
+              semester: academic.semester,
+              courseNo: course.courseNo,
+              courseName: course.courseName,
+              type: course.type,
+              sections: [],
+            };
+            if (course.type != COURSE_TYPE.SEL_TOPIC) {
+              const [tqf3, tqf5] = await Promise.all([
+                this.tqf3Model.create({ status: TQF_STATUS.NO_DATA }),
+                this.tqf5Model.create({ status: TQF_STATUS.NO_DATA }),
+              ]);
+              newCourse.TQF3 = tqf3.id;
+              newCourse.TQF5 = tqf5.id;
+            } else {
+              dataSection.TQF3 = (
+                await this.tqf3Model.create({ status: TQF_STATUS.NO_DATA })
+              ).id;
+              dataSection.TQF5 = (
+                await this.tqf5Model.create({ status: TQF_STATUS.NO_DATA })
+              ).id;
+            }
+            newCourse.sections.push(dataSection);
+            newCourses.push(newCourse as Course);
+            existingCourse = newCourse as Course;
+          } else if (isCreate && existingCourse) {
+            if (course.type == COURSE_TYPE.SEL_TOPIC) {
+              const existTopicSection = existingCourse.sections.find(
+                (item) => item.topic === section.topic,
+              );
+              if (existTopicSection) {
+                dataSection.TQF3 = existTopicSection.TQF3;
+                dataSection.TQF5 = existTopicSection.TQF5;
+              } else {
+                dataSection.TQF3 = (
+                  await this.tqf3Model.create({ status: TQF_STATUS.NO_DATA })
+                ).id;
+                dataSection.TQF5 = (
+                  await this.tqf5Model.create({ status: TQF_STATUS.NO_DATA })
+                ).id;
+              }
+            }
+            existingCourse.sections.push(dataSection);
+          }
+          console.log('Section Added:', dataSection);
+        }
+      }
+
+      await this.courseModel.create(newCourses);
+
+      return academic;
     } catch (error) {
       throw error;
     }
