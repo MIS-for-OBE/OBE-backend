97e73810280533d223f73dee62400cae836e80fa
worapit <drm.wmwork@gmail.com>
2025-01-23 20:12:16 +0700

    fix error

--- src/obe/tqf5/tqf5.module.ts
+++ src/obe/tqf5/tqf5.module.ts
@@ -3,10 +3,14 @@ import { TQF5, TQF5Schema } from './schemas/tqf5.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TQF5Controller } from './tqf5.controller';
 import { TQF5Service } from './tqf5.service';
+import { TQF3, TQF3Schema } from '../tqf3/schemas/tqf3.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: TQF5.name, schema: TQF5Schema }]),
+    MongooseModule.forFeature([
+      { name: TQF5.name, schema: TQF5Schema },
+      { name: TQF3.name, schema: TQF3Schema },
+    ]),
   ],
   controllers: [TQF5Controller],
   providers: [TQF5Service],
